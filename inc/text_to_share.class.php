793d4aef1ca21f355075ada6a308c7e780e69792
webdisrupt <webdisrupt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-05 22:20:50 +0000

    minor changes to css
    git-svn-id: https://plugins.svn.wordpress.org/text-to-share/trunk@1083464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/text_to_share.class.php
+++ inc/text_to_share.class.php
@@ -3,7 +3,7 @@
   Plugin Name: Text To Share
   Plugin URI: http://www.webdisrupt.com/wordpress-text-to-share-plugin
   Description: Main class for plugin that allows a piece of text to be shared across social platforms.
-  Version: 0.3
+  Version: 0.4
   Author: Web Disrupt
   Author URI: http://www.webdisrupt.com
   License: GNU General Public License v2
@@ -94,7 +94,7 @@ class Text_To_Share
 			$html_to_share = '<div class="text-to-share"><blockquote><p class="text-to-share-content">'.$content.'</p></blockquote>';
 			$html_to_share .= '<div class="text-to-share-buttons"><a href="'.$this->tts_replace_url($content, TEXT_TO_SHARE_TWITTER_URL).'" title="Share on Twitter" class="text-to-share-twitter"><span>'.TEXT_TO_SHARE_TWEET_TEXT.'</span></a>';
 			$html_to_share .= '<a href="'.$this->tts_replace_url($content, TEXT_TO_SHARE_FACEBOOK_URL).'" title="Share on Facebook" class="text-to-share-facebook"><span>'.TEXT_TO_SHARE_FB_TEXT.'</span></a>
-			<p class="text-to-share-credits"><a href="http://webdisrupt.com" target="_blank">Wordpress Text To Share Plugin by webdisrupt.com</a></p>
+			<p class="text-to-share-credits"><a href="http://webdisrupt.com" rel="nofollow" target="_blank">Wordpress Plugin by webdisrupt.com</a></p>
 			</div></div>';
 		}
 		
